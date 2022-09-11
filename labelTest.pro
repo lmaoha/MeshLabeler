49a033a67346bc561cdcb3712e762d6d1135b4db
lmaoha <70691676+lmaoha@users.noreply.github.com>
2022-09-11 15:27:10 +0800

    test

--- labelTest.pro
+++ labelTest.pro
@@ -1,4 +1,4 @@
-﻿QT       += core gui
+QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     vtkOutputWindow::SetGlobalWarningDisplay(0);
+
+
     MainWindow w;
     w.show();
     return a.exec();
